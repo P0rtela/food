b0b1899f0ce81bced5d40b918cf52ab69ff16e15
Portela <lucasplpmendonca@gmail.com>
2022-06-10 23:32:04 -0300

    completo

---
+++ food.js
@@ -0,0 +1,28 @@
+function comida() {
+    let x = Math.floor(Math.random() * 23);
+    var foo = document.getElementById("food");
+    
+    if (x==0){location.href="berinjela.html";} else
+    if (x==1){location.href="brocolis.html";} else
+    if (x==2){location.href="carambola.html";} else
+    if (x==3){location.href="chimarrao.html";} else
+    if (x==4){location.href="chocolate.html";} else
+    if (x==5){location.href="coxinha.html";} else
+    if (x==6){location.href="cuscuz.html";} else
+    if (x==7){location.href="feijoada.html";} else
+    if (x==8){location.href="frango.html";} else
+    if (x==9){location.href="goiaba.html";} else
+    if (x==10){location.href="jujuba.html";} else
+    if (x==11){location.href="kibe.html";} else
+    if (x==12){location.href="laranja.html";} else
+    if (x==13){location.href="limao.html";} else
+    if (x==14){location.href="macarrao.html";} else
+    if (x==15){location.href="maracuja.html";} else
+    if (x==16){location.href="mexirica.html";} else
+    if (x==17){location.href="pao_de_queijo.html";} else
+    if (x==18){location.href="pao.html";} else
+    if (x==19){location.href="pastel.html";} else
+    if (x==20){location.href="pepino.html";} else
+    if (x==21){location.href="queijo.html";} else
+    if (x==22){location.href="tomate.html";}
+}
